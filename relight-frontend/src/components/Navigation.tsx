7c747161bf3f4ba07dc24bb532ed973349e16ca3
kkumber <angelbertreyes28@gmail.com>
2025-01-11 18:39:08 -0800

    added navbar

--- relight-frontend/src/components/Navigation.tsx
+++ relight-frontend/src/components/Navigation.tsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 const Navigation = () => {
   return (
     <nav>
-        <Link to='' />
+        <Link to='/'>Homepage</Link>
+        <Link to='/register'>Register</Link>
+        <Link to='/login'>Login</Link>
     </nav>
   );
 };
